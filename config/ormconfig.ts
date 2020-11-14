256db1583dac50d60f4a38e2214c7c6ff92ffc27
liuwanyong2017 <2674706698@qq.com>
2020-11-14 20:14:34 +0800

    实现了orm的数据库的user表module的创建和导入到orm里，注意，entities导入这些表的实例用静态文件地址的话，需要在app.module.ts的文件路径的基础上设置全局路径。

--- config/ormconfig.ts
+++ config/ormconfig.ts
@@ -1,11 +1,16 @@
-export const ormconfig = {
-    port: 3306,
-    host: "localhost",
-    type: "mysql",
-    username: "root",
-    password: "kk201101",
-    database: "nest",
-    synchronize: true,  //同步的
-    logging: true,
-    entities: ["src/**/*.entity{.ts,.js}"]  //数据表的文件模块的导入！
-};
+import {TypeOrmModule} from "@nestjs/typeorm";
+
+
+export const mysqlOrm = TypeOrmModule.forRoot(
+    {
+        port: 3306,
+        host: "localhost",
+        type: "mysql",
+        username: "root",
+        password: "kk201101",
+        database: "nest",
+        synchronize: true,  //同步的
+        logging: true,
+        entities: ["../src/**/*.entity{.ts,.js}"]  //数据表的文件模块的导入！，全局静态文件地址。需要看app.module.ts文件的路径
+    }
+);
---
+++ src/business/user/user.entity.ts
@@ -0,0 +1,19 @@
+import {Entity, Column, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column({default: false})
+    deleted: boolean;
+
+    @Column({default: 5})
+    auth: number;
+
+    @Column()
+    password: string;
+}
